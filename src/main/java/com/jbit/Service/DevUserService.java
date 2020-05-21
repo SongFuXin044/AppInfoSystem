639bb6022d232647442b078667352aa209322bef
Emenn <Emenn_@outlook.com>
2020-05-21 17:22:19 +0800

    Idea Commit

---
+++ src/main/java/com/jbit/Service/DevUserService.java
@@ -0,0 +1,24 @@
+package com.jbit.Service;
+
+import com.jbit.mapper.DevUserMapper;
+import com.jbit.pojo.DevUser;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+@Service
+@Transactional(propagation = Propagation.SUPPORTS)
+public class DevUserService  {
+    @Resource
+    private DevUserMapper devUserMapper;
+
+
+    public DevUser queryLogin(String devcode , String devpassword){
+    DevUser user =new DevUser();
+    user.setDevcode(devcode);
+    user.setDevpassword(devpassword);
+    return devUserMapper.selectOne(user);
+    }
+}
